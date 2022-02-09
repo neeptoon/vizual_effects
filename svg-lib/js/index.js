6ea178ad02027cddf5fa015ca647659c2392354f
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-02-09 20:49:04 +0300

    add library svg.js

--- svg-lib/js/index.js
+++ svg-lib/js/index.js
@@ -1,7 +1,2 @@
-console.log('hellog')
-
-import {SVG} from '../libraries/svg.js';
-
-
-var draw = SVG().addTo('body').size(300, 300)
-var rect = draw.rect(100, 100).attr({ fill: '#f06' })
+var draw = SVG().addTo('body').size(500, 900)
+var rect = draw.rect(100, 250).attr({ fill: 'blue' })
